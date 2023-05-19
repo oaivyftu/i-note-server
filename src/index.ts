7eb2efdc5087356975cc17c271e75f57c4c69e8b
Le Oai Vy <oaivyftu@gmail.com>
2023-05-19 08:50:23 +0700

    fix: add sync route

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cors from 'cors'
 import 'dotenv/config'
 import * as process from "process";
+import { sequelize } from "./models";
 
 const app: Express = express()
 const port = process.env.NODE_ENV === "development" ? 3001 : process.env.PORT || 3001
@@ -14,6 +15,13 @@ app.use(bodyParser.json())
 
 app.get('/', async(req: Request, res: Response): Promise<Response> => {
     return res.status(200).send({ message: `Welcome to the inote API! \n Endpoints available at http://localhost:${port}/api/v1` })
+})
+
+app.get('/sync', async (req, res) => {
+    try {
+        await sequelize.sync({force:true})
+        res.send('database sync completed')
+    } catch (error) {}
 })
 
 app.use('/api/v1', router)
