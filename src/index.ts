0f7fb1b455b7b109f98aff95ab6e683915d22681
Le Oai Vy <oaivyftu@gmail.com>
2023-05-19 11:41:14 +0700

    feat: add husky

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,37 @@
-import express, { Express, Request, Response } from 'express';
-import router from "./routes";
+import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
-import cors from 'cors'
-import 'dotenv/config'
-import * as process from "process";
+import cors from "cors";
+import "dotenv/config";
 import { sequelize } from "./models";
+import router from "./routes";
 
-const app: Express = express()
-const port = process.env.NODE_ENV === "development" ? 3001 : process.env.PORT
+const app: Express = express();
+const port = process.env.NODE_ENV === "development" ? 3001 : process.env.PORT;
 
-app.use(cors())
-app.use(express.static(`${__dirname}/public`))
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json());
 
-app.get('/', async(req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send({ message: `Welcome to the inote API! \n Endpoints available at http://localhost:${port}/api/v1` })
-})
+app.get("/", async (req: Request, res: Response): Promise<Response> => {
+  return res
+    .status(200)
+    .send({
+      message: `Welcome to the inote API! \n Endpoints available at http://localhost:${port}/api/v1`,
+    });
+});
 
-app.get('/sync', async (req, res) => {
-    try {
-        await sequelize.sync({force:true})
-        res.send('database sync completed')
-    } catch (error) {}
-})
+app.get("/sync", async (req, res) => {
+  try {
+    await sequelize.sync({ force: true });
+    res.send("database sync completed");
+  } catch (e) {
+    const error = e as Error;
+    console.log(error.message);
+  }
+});
 
-app.use('/api/v1', router)
+app.use("/api/v1", router);
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at port ${port}`)
-})
+  console.log(`⚡️[server]: Server is running at port ${port}`);
+});
--- src/middlewares/verify-token.ts
+++ src/middlewares/verify-token.ts
@@ -3,16 +3,14 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export const verify = (req: Request, res: Response, next: NextFunction) => {
-  const auth = req.header('Authorization');
-  if (!auth)
-    return res.status(401).send('Access denied!!!')
-  let token = auth.split(' ')[1];
-  if (!token)
-    return res.status(401).send('Access denied!!!')
+  const auth = req.header("Authorization");
+  if (!auth) return res.status(401).send("Access denied!!!");
+  const token = auth.split(" ")[1];
+  if (!token) return res.status(401).send("Access denied!!!");
   try {
     jwt.verify(token, process.env.JWT_SECRET!);
-    next()
+    next();
   } catch (err) {
-    return res.status(400).send('Invalid token!!!')
+    return res.status(400).send("Invalid token!!!");
   }
-}
+};
